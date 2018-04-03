83b2914a604b0b7af3d833880e2a8f8101641029
LegoMaster3650 <37251737+LegoMaster3650@users.noreply.github.com>
2018-04-03 17:17:56 -0500

    Create index.php

---
+++ CPvApp/index.php
@@ -0,0 +1,25 @@
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+ <form id ="colors" name="colors">
+ <!-- Just a simple form with three radio buttons. Note that there is no sumbit button or action attribute.-->
+ <input type="radio" name="color" value="red" onclick="chooseColor(‘red’)">RED<br/>
+ <input type="radio" name="color" value="green" onclick="chooseColor(‘green’)">GREEN<br/>
+ <input type="radio" name="color" value="blue" onclick="chooseColor(‘blue’)">BLUE<br/>
+ </form>
+ function chooseColor(theValue) {
+    if (theValue != '') {
+        color = theValue;
+        TouchCastvApps.doAuthoringAction({
+            action: 'widgetIsReadyToGoLive',
+            value: 1
+        });
+    }
+}
+TouchCastvApps.doAuthoringAction({
+    action: 'widgetIsReadyToGoLive',
+    value: 1
+});
+function WidgetIsAboutToGoLive(){
+    // !!!BE SURE TO REPLACE THIS URL TO MATCH YOUR ACTUAL DOMAIN 
+    var theUrl = "http://developers.touchcast.com/examples/vapps/colorvapp/live.php?color="+color;
+    return '{"url":"'+theUrl+'","reload-page":"1"}';
+}
