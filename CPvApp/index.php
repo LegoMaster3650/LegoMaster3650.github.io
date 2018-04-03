29456e0e9880b2954aae88c890a2ee4f124ecb2c
LegoMaster3650 <37251737+LegoMaster3650@users.noreply.github.com>
2018-04-03 17:34:24 -0500

    Update index.php

--- CPvApp/index.php
+++ CPvApp/index.php
@@ -16,7 +16,7 @@
 			function WidgetIsAboutToGoLive(){
 
 				// !!!BE SURE TO REPLACE THIS URL TO MATCH YOUR ACTUAL DOMAIN 
-				var theUrl = "http://developers.touchcast.com/examples/vapps/colorvapp/live.php?color="+color;
+				var theUrl = "http://legomaster3650.github.io/CPvApp/live.php?color="+color;
 				return '{"url":"'+theUrl+'","reload-page":"1"}';
 
 			}						
