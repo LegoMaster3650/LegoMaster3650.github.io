ab85c3e82408a5d27c49851fc71743b7e41a68ef
LegoMaster3650 <37251737+LegoMaster3650@users.noreply.github.com>
2018-04-03 17:25:45 -0500

    Update index.php

--- CPvApp/index.php
+++ CPvApp/index.php
@@ -1,10 +1,12 @@
 <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+
  <form id ="colors" name="colors">
  <!-- Just a simple form with three radio buttons. Note that there is no sumbit button or action attribute.-->
  <input type="radio" name="color" value="red" onclick="chooseColor(‘red’)">RED<br/>
  <input type="radio" name="color" value="green" onclick="chooseColor(‘green’)">GREEN<br/>
  <input type="radio" name="color" value="blue" onclick="chooseColor(‘blue’)">BLUE<br/>
  </form>
+
  function chooseColor(theValue) {
     if (theValue != '') {
         color = theValue;
@@ -14,10 +16,8 @@
         });
     }
 }
-TouchCastvApps.doAuthoringAction({
-    action: 'widgetIsReadyToGoLive',
-    value: 1
 });
+
 function WidgetIsAboutToGoLive(){
     // !!!BE SURE TO REPLACE THIS URL TO MATCH YOUR ACTUAL DOMAIN 
     var theUrl = "http://http://legomaster3650.github.io/CPvApp/index.php?color="+color;
