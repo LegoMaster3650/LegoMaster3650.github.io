54922fa41616c45ebbf74525a22ef55c94c53645
LegoMaster3650 <37251737+LegoMaster3650@users.noreply.github.com>
2018-04-03 17:19:31 -0500

    Create live.php

---
+++ CPvApp/live.php
@@ -0,0 +1,4 @@
+function WidgetIsAboutToSwitchToEditing(){
+    // !!!BE SURE TO REPLACE THIS URL TO MATCH YOUR ACTUAL DOMAIN 
+    return '{"url":"http://legomaster3650.github.io/CPvApp/live.php","reload-page":"1"}';
+}
